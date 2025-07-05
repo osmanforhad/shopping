3b7bed663852a9501b0c2434a9343e2cf5367f65
osmanforhad <osmanforhad93@gmail.com>
2025-07-05 18:00:17 +0600

    design Home and Details screen and extract data from data bank

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, View } from 'react-native'
 import React from 'react'
+//Navigation
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { RootStackParamList } from '../App'
+//Components
+import ProductItem from '../components/ProductItem'
+import Seperator from '../components/Seperator'
+//data
+import { PRODUCTS_LIST } from '../data/constants'
 
-const Home = () => {
+//type setup for Home navigation props
+type HomeProps = NativeStackScreenProps<RootStackParamList, "Home">
+
+const Home = ({ navigation }: HomeProps) => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <FlatList
+                data={PRODUCTS_LIST}
+                keyExtractor={item => item.id}
+                ItemSeparatorComponent={Seperator}
+                renderItem={({ item }) => (
+                    <Pressable onPress={() => {
+                        navigation.navigate('Details', {
+                            product: item
+                        })
+                    }}>
+                        <ProductItem product={item} />
+                    </Pressable>
+                )}
+            />
         </View>
     )
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+
+        padding: 12,
+        backgroundColor: '#FFFFFF',
+    },
+});
 
 export default Home;
-
-const styles = StyleSheet.create({});
